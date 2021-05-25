cc9942ff4448af325b668062971b5ae91ee43222
xmflswood <774898896@qq.com>
2021-05-25 14:05:59 +0800

    rebuild

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? '/pinyin-match' : '/',
-    productionSourceMap: false
+    productionSourceMap: false,
+    chainWebpack: config => {
+        config
+          .plugin('html')
+          .tap(args => {
+            args[0].minify = false
+            return args
+          })
+      }
 }
